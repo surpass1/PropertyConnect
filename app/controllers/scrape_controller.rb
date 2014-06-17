b042742145134803bc2ac1897e974661439e43c7
atusasire <atusasirelucas@gmail.com>
2014-06-17 22:04:26 +0300

    Added scrapping controller

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -11,8 +11,10 @@ class ScrapeController < ApplicationController
 
 	@title = doc.xpath("//div/h4").collect {|node| node.text.strip}
 	@price = doc.css(".ns2-introtext").collect {|node| node.text.strip}
+
 	@image_url = doc.css('.ns2-image').collect {|node| node.get_attribute(:src)}
 	@image = File.open("@image_url", "wb")
+	
 	
 
   end

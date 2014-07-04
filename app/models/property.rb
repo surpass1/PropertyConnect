04b48646d86f82233273ac0660e00589c613c601
atusasire <atusasirelucas@gmail.com>
2014-07-04 21:41:16 +0300

    removed dead code

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,16 +11,6 @@ class Property < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-=begin
-  
-rescue Exception => e
-  
-end
-  def self.search(search, page)
-  paginate :per_page => 5, :page => page,
-           :conditions => ['name location ?', "%#{search}%"], :order => 'location'
-  end
-=end
   def self.search(search, page)
     conditions = nil
     unless search.blank?
@@ -28,23 +18,4 @@ end
     end
     paginate :per_page => 5, :page => page, :conditions => conditions, :order => 'location'
   end
-
-  def scrape
-    url = "http://www.nhcc.co.ug/"
-      doc = Nokogiri::HTML(open(url))
-    
-    title = []
-    price = []
-    
-    title = doc.xpath("//div/h4").collect {|node| node.text.strip}
-    price = doc.css(".ns2-introtext").collect {|node| node.text.strip}
-    puts title
-    puts price
-
-    #Property.create(:title => title, :price => price)
-
-    nhcc = Property.new(title: @title, price: @price)
-    nhcc.save
-
- end
 end

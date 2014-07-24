957e11af0dc23a52e9ef6fd865195ad377ce938d
atusasire <atusasirelucas@gmail.com>
2014-07-24 12:15:05 +0300

    Added image on the user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,25 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-	if current_user.nil? # user is not logged in
-	session[:next] = request.fullpath
-	redirect_to login_url,
-	:alert => "Please log in to continue."
-	else
-		
-	if request.env["HTTP_REFERER"].present?
-	redirect_to :back, :alert => exception.message
-	else
-	render :file => "#{Rails.root}/app/views/home.html.erb",
-	:status => 403, :layout => false
+		if current_user.nil? # user is not logged in
+		session[:next] = request.fullpath
+		redirect_to login_url,
+		:alert => "Please log in to continue."
+		else
+				
+			if request.env["HTTP_REFERER"].present?
+			redirect_to :back, :alert => exception.message
+			else
+			render :file => "#{Rails.root}/app/views/home.html.erb",
+			:status => 403, :layout => false
+			end
+		end
 	end
-	end
-	end
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140724081152) do
+ActiveRecord::Schema.define(:version => 20140724085411) do
 
   create_table "blog_comments", :force => true do |t|
     t.string   "name",       :null => false
@@ -125,6 +125,13 @@ ActiveRecord::Schema.define(:version => 20140724081152) do
     t.datetime "updated_at",                                       :null => false
     t.string   "role",                   :default => "registered"
     t.integer  "roles_mask"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "name"
+    t.datetime "date_of_birth"
+    t.boolean  "is_female",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
